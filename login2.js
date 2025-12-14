252eb6738f8f8431b442bd84848a09831ddb3cd1
your-name <your-email>
2025-12-15 00:38:36 +0500

    Fix CORS for frontend connection

--- login2.js
+++ login2.js
@@ -10,7 +10,7 @@ async function login() {
 
   try {
     const res = await fetch(
-      "https://snooker-backend-grx0.onrender.com/api/auth/login",
+      "https://snooker-backend-grx6.onrender.com/api/auth/login",
       {
         method: "POST",
         headers: {
